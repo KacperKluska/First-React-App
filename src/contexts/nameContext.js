47da6c742f4c7dd41ef5a68db0d280f50f0e3da9
KacperKluska <kacperkluska130@gmail.com>
2021-07-21 11:30:19 +0200

    context created

--- src/contexts/nameContext.js
+++ src/contexts/nameContext.js
@@ -1,3 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const NameContext = React.createContext("");
+
+export const NameContextProvider = (props) => {
+  const [name, setName] = useState("");
+
+  return (
+    <NameContext.Provider value={[name, setName]}>
+      {props.children}
+    </NameContext.Provider>
+  );
+};
