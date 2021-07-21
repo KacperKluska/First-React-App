4c9d159fdae59a579bb9d83d71c9056a3c12b82e
KacperKluska <kacperkluska130@gmail.com>
2021-07-21 12:37:35 +0200

    style added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Name from './views/nameView'
 import { NameContextProvider } from './contexts/nameContext'
+import './index.css'
 
 function App() {
   return (
-    <div>
+    <div id="container">
       <NameContextProvider>
         <Name greeting={"Hello"}/>
       </NameContextProvider>
