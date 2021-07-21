56e5dbf7fac83384fbd09da5a2a5ef6ef7e212ad
KacperKluska <kacperkluska130@gmail.com>
2021-07-21 11:40:20 +0200

    App.js modified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Name from './views/nameView'
+import NameContextProvider from './contexts'
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <Name greeting={"Hello"}/>
+    </div>
+  );
 }
 
 export default App;
